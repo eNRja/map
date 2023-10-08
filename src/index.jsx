95ed4065e91e81eaeaffe97a92dec86189a2b934
Алексей <satukov90@yandex.ru>
2023-10-08 10:54:06 +0300

    map

---
+++ src/index.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import ReactMapboxGl from "react-mapbox-gl";
+import DrawControl from "react-mapbox-gl-draw";
+import "@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css";
+
+import "./styles.css";
+
+const Map = ReactMapboxGl({
+  accessToken:
+    "pk.eyJ1IjoiZmFrZXVzZXJnaXRodWIiLCJhIjoiY2pwOGlneGI4MDNnaDN1c2J0eW5zb2ZiNyJ9.mALv0tCpbYUPtzT7YysA2g",
+});
+
+function App() {
+  const onDrawCreate = ({ features }) => {
+    console.log(features);
+  };
+
+  const onDrawUpdate = ({ features }) => {
+    console.log(features);
+  };
+
+  return (
+    <div className="map-container">
+      <Map
+        style="mapbox://styles/mapbox/streets-v9" // eslint-disable-line
+        containerStyle={{
+          height: "100vh",
+          width: "100vw",
+        }}
+      >
+        <DrawControl onDrawCreate={onDrawCreate} onDrawUpdate={onDrawUpdate} />
+      </Map>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
